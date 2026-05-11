341fc03014153ed1d1a21d54a84445353acd4070
Mitch Anderson <mitch@undeadops.xyz>
2026-05-11 12:44:34 -0600

    chore: fixing otel package version

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -8,7 +8,7 @@ import (
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 )
 
 const endpointEnvVar = "OTEL_EXPORTER_OTLP_ENDPOINT"
